e8b03af35e96be54231502ca608a6814eb01b66d
admltf <adamlatif4@hotmail.com>
2021-11-13 22:23:39 +0000

    added lesson

---
+++ lessons.js
@@ -0,0 +1,20 @@
+lessons = [
+    {
+        id: 100,
+        title: "Math",
+        location: "London",
+        price: 100,
+        image: "mathimg.png",
+        spaces: 5
+    },
+
+    {
+        id: 101,
+        title: "English",
+        location: "London",
+        price: 100,
+        image: "engimg.png",
+        spaces: 5,
+    },
+    
+]
