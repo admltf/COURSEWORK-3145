6e1dd7f169d6ddfd1668e6ccda981fb0fbd6e136
admltf <adamlatif4@hotmail.com>
2021-11-13 23:08:57 +0000

    all lesson images

--- lessons.js
+++ lessons.js
@@ -4,7 +4,7 @@ lessons = [
         title: "Math",
         location: "London",
         price: 100,
-        image: "mathimg.png",
+        image: "../images/mathimg.png",
         spaces: 5
     },
 
@@ -13,8 +13,79 @@ lessons = [
         title: "English",
         location: "London",
         price: 100,
-        image: "engimg.png",
+        image: "../images/engimg.png",
         spaces: 5,
     },
-    
+
+    {
+        id: 102,
+        title: "History",
+        location: "London",
+        price: 100,
+        image: "../images/hisimg.png",
+        spaces: 5,
+    },
+
+    {
+        id: 103,
+        title: "Geography",
+        location: "London",
+        price: 100,
+        image: "../images/geoimg.png",
+        spaces: 5,
+    },
+
+    {
+        id: 104,
+        title: "Computer Science",
+        location: "London",
+        price: 100,
+        image: "../images/comimg.png",
+        spaces: 5,
+    },
+
+    {
+        id: 105,
+        title: "Art",
+        location: "London",
+        price: 100,
+        image: "../images/artimg.png",
+        spaces: 5,
+    },
+
+    {
+        id: 106,
+        title: "Music",
+        location: "London",
+        price: 100,
+        image: "../images/musimg.png",
+        spaces: 5,
+    },
+
+    {
+        id: 107,
+        title: "Sports",
+        location: "London",
+        price: 100,
+        image: "../images/spoimg.png",
+        spaces: 5,
+    },
+
+    {
+        id: 108,
+        title: "Science",
+        location: "London",
+        price: 100,
+        image: "../images/sciimg.png",
+        spaces: 5,
+    },
+
+    {
+        id: 109,
+        title: "Drama",
+        location: "London",
+        price: 100,
+        image: "../images/draimg.png",
+        spaces: 5,
+    },   
 ]
